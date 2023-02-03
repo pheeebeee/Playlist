475d60682cceb5412e25e48ab12f0bd815c73875
Phoebe Salvati <psalvati23@students.polytechnic.org>
2023-02-02 17:17:01 -0800

    tester to print remove songs and print liked songs

--- PlaylistTester.java
+++ PlaylistTester.java
@@ -60,11 +60,13 @@ public class PlaylistTester {
 
         System.out.println("\nPrinting only the liked songs...\n");
         //Your Playlist should be able to do this without looping while in main!
+        summary = p.examineLikedSongs();
+        System.out.println(summary);
 
 
         System.out.println("\nPrinting the total duration of all songs...\n");
         //Note that the format should look something like minutes:seconds
-
+        
 
         System.out.println("\nRemoving all unliked songs from the playlist...\n");
         //This should be doable with a single method call
